1d5ae0143a2768c18e49d4b6f55501935a46508c
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-22 14:28:36 -0300

    Configuracion del servidor

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('desde indexx.js')
+const express = require('express');
+
+// Creamos el servidor
+const app = express();
+
+// Definimos ruta principal
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+})
+
+app.listen(4000, () => {
+    console.log('El servidor esta corriendo perfectamente')
+})
