e84f31fd2103fefa6802d0755be0e0fbd7bf8c87
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-22 16:40:22 -0300

    Controllador producto

---
+++ controllers/productoController.js
@@ -0,0 +1,5 @@
+
+
+exports.crearProducto = (req, res) => {
+    console.log('desde crear producto');
+}
--- routes/producto.js
+++ routes/producto.js
@@ -1,10 +1,9 @@
 // Rutas para producto
 const express = require('express');
 const router = express.Router();
+const productoController = require('../controllers/productoController');
 
 // api/productos
-router.post('/', () => {
-    console.log('Creando producto..');
-})
+router.post('/', productoController.crearProducto);
 
 module.exports = router;
