bee60a6b13b4d79a6bc1c3072509b268d5d6266b
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-25 08:44:24 -0300

    Enviamos datos a la API

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,5 +1,5 @@
 
 
 exports.crearProducto = (req, res) => {
-    console.log('desde crear producto');
+    console.log(req.body);
 }
--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const app = express();
 // Conectamos a la BD
 conectarDB();
 
-app.use('/api/productos', require('./routes/producto'));
+app.use(express.json());
 
-// Definimos ruta principal
-/* app.get('/', (req, res) => {
-    res.send('Hola Mundo');
-}) */
+app.use('/api/productos', require('./routes/producto'));
 
 app.listen(4000, () => {
     console.log('El servidor esta corriendo perfectamente')
