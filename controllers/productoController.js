41ebd61f22aa7ea95f73ae4131646060ce4b3cc6
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-25 09:58:24 -0300

    Obtener producto

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -55,3 +55,21 @@ exports.actualizarProducto = async (req, res) => {
         res.status(500).send('Hubo un error');
     }
 }
+
+
+exports.obtenerProducto = async (req, res) => {
+
+    try {
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto) {
+            res.status(404).json({ msg: 'No existe el producto' })
+        }
+       
+        res.json(producto);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
--- routes/producto.js
+++ routes/producto.js
@@ -7,5 +7,6 @@ const productoController = require('../controllers/productoController');
 router.post('/', productoController.crearProducto);
 router.get('/', productoController.obtenerProductos);
 router.put('/:id', productoController.actualizarProducto);
+router.get('/:id', productoController.obtenerProducto);
 
 module.exports = router;
