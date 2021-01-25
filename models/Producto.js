757294160d4841de9a5266b659cb416ecaab82d3
truizdiaz <tomiruizdiaz@gmail.com>
2021-01-25 08:59:00 -0300

    Creacion del modelo con mongoose

---
+++ models/Producto.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const ProductoSchema = mongoose.Schema({
+    producto: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    ubicacion: {
+        type: String,
+        required: true
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    fechaCreacion: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Producto', ProductoSchema);
